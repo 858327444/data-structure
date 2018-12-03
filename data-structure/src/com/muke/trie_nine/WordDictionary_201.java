cdf6859d471bc0b84a9f82698dfbccc7687f15dc
yanlp <858327444@qq.com>
2018-12-03 22:25:11 +0800

    提交一次试试

---
+++ data-structure/src/com/muke/trie_nine/WordDictionary_201.java
@@ -0,0 +1,97 @@
+package com.muke.trie_nine;
+
+import java.util.TreeMap;
+
+/**
+ * 设计一个支持以下两种操作的数据结构：
+ * void addWord(word)
+ * bool search(word)
+ * search(word) 可以搜索文字或正则表达式字符串，字符串只包含字母 . 或 a-z 。 . 可以表示任何一个字母。
+ * 示例:
+ * addWord("bad")
+ * addWord("dad")
+ * addWord("mad")
+ * search("pad") -> false
+ * search("bad") -> true
+ * search(".ad") -> true
+ * search("b..") -> true
+ * 说明:
+ * 你可以假设所有单词都是由小写字母 a-z 组成的。
+ * Program Name: data-structure
+ * Created by yanlp on 2018/12/2
+ *
+ * @author yanlp
+ * @version 1.0
+ */
+public class WordDictionary_201 {
+    private class Node {
+        // 当前这个节点,是否代表这个单词的结尾,即是否访问到一个单词了
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary_201() {
+        root = new Node();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            // 没有存储过该字符
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        // 循环结束后,最后一个节点的isWord需要设置为true
+        if (!cur.isWord) {
+            cur.isWord = true;
+        }
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(Node node, String word, int index) {
+        if (index == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(index);
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            } else {
+                return match(node.next.get(c), word, index + 1);
+            }
+        } else {
+            for (char nextChar : node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
